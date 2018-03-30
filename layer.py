1ef05bbca69cb98bf597838122057ff5fac04202
zhangjiamin <zhangjiamin@nuctech.com>
2018-03-30 16:57:02 +0800

    modify layer.py

--- layer.py
+++ layer.py
@@ -18,7 +18,15 @@ class Layer:
         pass
 
     def Forward(self, bottom, top):
-        pass
+        loss = 0
+        self.Reshape(bottom, top)
+        self.Forward_cpu(bottom, top)
+
+        for top_id in range(len(top)):
+            if 0 != self.loss(top_id)
+                loss = loss + numpy.dot(top[top_id].diff(), top[top_id].data)
+
+        return loss
 
     def Backward(self, top, propagate_down, bottom):
         pass
@@ -71,7 +79,24 @@ class Layer:
     def set_param_propagate_down(self, param_id, value): 
         self.param_propagate_down_[param_id] = value
 
-    
+    def Forward_cpu(self, bottom, top):
+        pass
+
+    def Forwad_gpu(self, bottom, top):
+        pass
+
+    def Backward_cpu(self, top, propagate_down, bottom):
+        pass
+
+    def Backward_gpu(self, top, propagate_down, bottom):
+        pass
+
+    def CheckBlobCounts(self, bottom, top):
+        pass
+
+    def SetLossWeights(self, top):
+        pass
+
 
 if __name__ == '__main__':
     blob  = Blob(numpy.float, (2,3))
