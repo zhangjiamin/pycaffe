c5a3aa8d61e5246d7c90d4b894d2f0f6080160e9
zhangjiamin <zhangjiamin@nuctech.com>
2018-03-30 14:22:19 +0800

    modify blob.py

--- blob.py
+++ blob.py
@@ -4,19 +4,54 @@ class Blob:
 
     def __init__(self, dtype, shape):
         self.shape_ = shape
-        self.data_  = numpy.zeros(shape,dtype)
+        self.data_  = numpy.ones(shape, dtype)
+        self.diff_  = numpy.ones(shape, dtype)
 
     def reshape(self, shape):
         self.shape_ = shape
         self.data_  = numpy.reshape(self.data_, shape)
+        self.diff_  = numpy.reshape(self.diff_, shape)
+
+    def shape_string(self):
+        return str(self.shape_)
+
+    def shape(self):
+        return self.shape_
+
+    def shape_index(self, index):
+        return self.shape_[index]
+
+    def num_axes(self):
+        return self.data_.ndim
+
+    def count(self):
+        return self.data_.size
+
+    def data(self):
+        return self.data_
+
+    def diff(self):
+        return self.diff_
+
+    def scale_data(self, scale_factor):
+        self.data_ = self.data_ * scale_factor
+
+    def scale_diff(self, scale_factor):
+        self.diff_ = self.diff_ * scale_factor
 
 if __name__ == '__main__':
     blob = Blob(numpy.float, (5,6))
     print blob.data_
-    print blob.data_.shape
-    print blob.shape_
+    print blob.shape()
     blob.reshape((2,15))
     print blob.data_
-    print blob.data_.shape
-    print blob.shape_
+    print blob.shape()
+    print blob.shape_index(1)
+    print blob.shape_string()
+    print blob.num_axes()
+    print blob.count()
 
+    blob.scale_data(0.1)
+    blob.scale_diff(0.01)
+    print blob.data()
+    print blob.diff()
