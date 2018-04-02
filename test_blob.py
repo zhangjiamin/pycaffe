91c269809f84a56758153ebc19e39d1758ac008e
zhangjiamin <zhangjiamin@nuctech.com>
2018-04-02 10:42:16 +0800

    add testcase

---
+++ test_blob.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestBlob(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_1(self):
+        print 'test_1'
+
+    def test_2(self):
+        print 'test_2'
+
+if __name__ == '__main__':
+    unittest.main()
