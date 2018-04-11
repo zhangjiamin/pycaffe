0776a72dce2941bf8bda93c8467ee041c7005458
zhangjiamin <zhangjiamin@nuctech.com>
2018-04-11 16:56:39 +0800

    add max_pool_forward_naive & max_pool_backward_naive

---
+++ core/max_pool_backward_naive.py
@@ -0,0 +1,13 @@
+
+def max_pool_backward_naive(x, dout, pool_height, pool_width, stride):
+    N, C, H_out, W_out = dout.shape
+    dx = np.zeros(x.shape)
+
+    for n in range(N):
+        for c in range(C):
+            for h in range(H_out):
+                for w in range(W_out):
+                    current_matrix = x[n, c, h * stride:h * stride + pool_height, w * stride:w * stride + pool_width]
+                    max_idx = np.unravel_index(np.argmax(current_matrix),current_matrix.shape)
+                    dx[n, c, h * stride + max_idx[0], w * stride + max_idx[1]] += dout[n, c, h, w]
+
---
+++ core/max_pool_forward_naive.py
@@ -0,0 +1,15 @@
+
+def max_pool_forward_naive(x, pool_height, pool_width, stride):
+    N, C, H, W = x.shape
+    H_out = 1 + (H - pool_height) / stride
+    W_out = 1 + (W - pool_width) / stride
+    out = np.zeros((N, C, H_out, W_out))
+
+    for n in range(N):
+        for c in range(C):
+            for h in range(H_out):
+                for w in range(W_out):
+                    out[n, c, h, w] = np.max(x[n, c, h*stride:h*stride+pool_height, w*stride:w*stride+pool_width])
+
+    return out
+
