d2828546a4807ab53b1de66519486b15d91f7490
zhangjiamin <zhangjiamin@nuctech.com>
2018-04-15 21:34:13 +0800

    add net

--- core/net.py
+++ core/net.py
@@ -2,8 +2,29 @@
 class Net:
 
     def __init__(self):
+        self.layers_  = []
+        self.bottoms_ = []
+        self.tops_    = []
+        self.learnable_params_ = []
+
+    def Forward(self, loss):
         pass
 
+    def ClearParamDiffs(self):
+        pass
+
+    def Backward(self):
+        pass
+
+    def Reshape(self):
+        pass
+
+    def ForwardBackward(self):
+        pass
+
+    def Update(self):
+        pass
+ 
 
 if __name__ == '__main__':
     print 'Net Class'
