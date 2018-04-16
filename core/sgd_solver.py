be71bca0031c58988cdfb017c909acc85d260ad0
zhangjiamin <zhangjiamin@nuctech.com>
2018-04-16 17:50:03 +0800

    modify sgd_solver

--- core/sgd_solver.py
+++ core/sgd_solver.py
@@ -4,15 +4,24 @@ class SGDSolver(Solver):
 
     def __init__(self):
         Solver.__init__(self)
+        self.lr_ = 0.01
 
     def type(self):
         return 'SGD'
 
     def ApplyUpdate(self):
-        pass
+        rate = self.GetLearningRate()
+        self.ClipGradients()
+        params = self.net_.learnable_params()
+        for i in range(len(params)):
+            self.Normalize(params[i])
+            self.Regularize(params[i])
+            self.ComputeUpdateValue(params[i], rate)
+
+        self.net_.Update()
 
     def GetLearningRate(self):
-        pass
+        return self.lr_
 
     def Normalize(self, param):
         pass
