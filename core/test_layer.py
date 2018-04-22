1154fc645c2f7ebb84167117004eccfc50e0cec9
zhangjiamin <zhangjiamin@nuctech.com>
2018-04-22 11:15:46 +0800

    add test_solver.py

--- core/test_layer.py
+++ core/test_layer.py
@@ -183,56 +183,5 @@ class TestLayer(unittest.TestCase):
         print 'W.diff',layer.W.diff(),layer.W.data().shape
         print 'b.diff',layer.b.diff(),layer.b.data().shape
 
-    def test_mnist_mlp_net_solver(self):
-        train_net = Net()
-        test_net = Net()
-
-        bottom = Blob()
-        label  = Blob()
-        top    = Blob()
-        top1   = Blob()
-        top2   = Blob()
-        loss   = Blob()
-        top4   = Blob()
-        top5   = Blob()
-        top6   = Blob()
-        top7   = Blob()
-
-        batch_size = 100
-
-        test = MNISTTestDataLayer(batch_size)
-        train = MNISTTrainDataLayer(batch_size)
-        acc   = AccuracyLayer()
-
-        fc1  = InnerProductLayer(784,300)
-        relu = ReLULayer()
-        drop = DropoutLayer(0.75)
-        drop2 = DropoutLayer(1.0)
-        fc2  = InnerProductLayer(300,10)
-        softmaxloss = SoftmaxLossLayer()
-
-        train_net.AddLayer(train, [], [bottom,label])
-        train_net.AddLayer(fc1, [bottom], [top])
-        train_net.AddLayer(relu, [top], [top1])
-        train_net.AddLayer(drop, [top1], [top4])
-        train_net.AddLayer(fc2, [top4], [top2])
-        train_net.AddLayer(softmaxloss, [top2,label], [loss,top5])
-
-        test_net.AddLayer(test, [], [bottom,label])
-        test_net.AddLayer(fc1, [bottom], [top])
-        test_net.AddLayer(relu, [top], [top1])
-        test_net.AddLayer(drop2, [top1], [top4])
-        test_net.AddLayer(fc2, [top4], [top2])
-        test_net.AddLayer(softmaxloss, [top2,label], [loss,top5])
-        test_net.AddLayer(acc, [top5,label], [top6,top7])
-
-        test_net.AddOutputBlob(top6)
-        test_net.AddOutputBlob(top7)
-
-        solver = AdaDeltaSolver(0.1)
-        solver.AddTrainNet(train_net)
-        solver.AddTestNet(test_net)
-        solver.Solve(300)
-
 if __name__ == '__main__':
     unittest.main()
